65c62eaf436515eeead5c7c0dbb913c1216646f8
Anton Filatov <ya-enot@mail.ru>
2018-05-28 16:21:45 +0300

    Licensing changed to LGPLv3.0

--- core/api/src/main/java/network/tiesdb/handler/api/TiesHandlerFactory.java
+++ core/api/src/main/java/network/tiesdb/handler/api/TiesHandlerFactory.java
@@ -1,17 +1,20 @@
-/*
- * Copyright 2017 Ties BV
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * Copyright © 2017 Ties BV
+ *
+ * This file is part of Ties.DB project.
+ *
+ * Ties.DB project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ties.DB project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Ties.DB project. If not, see <https://www.gnu.org/licenses/lgpl-3.0>.
  */
 package network.tiesdb.handler.api;
 
